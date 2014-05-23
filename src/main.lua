f43b6ad4b019bc872a4fc223735cf591e6899567
Fabien Fleutot <fleutot@gmail.com>
2014-05-23 18:15:36 +0200

    Push logs in a dedicated file (TODO: create file if/when needed)

--- src/main.lua
+++ src/main.lua
@@ -14,13 +14,24 @@ bmv = assert(require 'victron.bmv'.new(BMV_DEV_FILE))
 require "bmv_web_pages"
 
 local function main()
-  log.setlevel('ALL', 'VICTRON-VED')
-  log("APP", "INFO", "BMV monitoring example")
+  log.setlevel('ALL', 'VICTRON-VED', 'APP')
+  log("APP", "INFO", " ***** STARTING APPLICATION ELORN_ENERGY *****")
   web.start(WEB_PORT)
   shell.init{ address='0.0.0.0', port=3000, editmode='edit', historysize=64 }
   datalog.init(bmv)
   datalog.start()
 end
- 
+
+-- Log in a file
+logfile = io.open('/var/log/elorn_energy', 'a')
+logfile :write('\n')
+
+function log.displaylogger(mod, level, msg)
+  logfile :write (msg..'\n')
+  logfile :flush()
+end
+log.setlevel('INFO', 'SCHED')
+log.setlevel('ALL')
+
 sched.run(main)
 sched.loop()
